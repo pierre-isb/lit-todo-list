d5074f5a958b3afa4f324fbf1807d6f60f6c1275
Pierre Isabel <pierre.isabel@octo.com>
2021-09-03 14:44:37 +0200

    first events testing

--- setup-tests.js
+++ setup-tests.js
@@ -1,1 +1,14 @@
 import "regenerator-runtime/runtime"
+import { oneEvent } from "@open-wc/testing"
+
+global.fireEvent = async (element, name, payload) => {
+  setTimeout(() => {
+    const event = new CustomEvent(name, {
+      bubbles: true,
+      composed: true,
+      detail: { message: payload },
+    })
+    element.dispatchEvent(event)
+  })
+  await oneEvent(element, name)
+}
--- src/components/__tests__/App.spec.js
+++ src/components/__tests__/App.spec.js
@@ -1,17 +1,29 @@
-import App from "../App"
+import "../App"
 import { expect, fixture, html } from "@open-wc/testing"
 
 let app
 let appName
 describe("App", () => {
   describe("Default", () => {
-    beforeAll(async () => {
+    beforeEach(async () => {
       app = await fixture(html`<todo-list></todo-list>`)
+    })
+    it("is accessible", () => {
+      expect(app).to.be.accessible()
     })
     it("contains a title with a flame image", () => {
       const title = app.shadowRoot.querySelector("h1")
       expect(title).to.exist
-      expect(title.innerHTML).to.include("<lit-flame></lit-flame>")
+      expect(title.innerHTML).to.contain("<lit-flame></lit-flame>")
+    })
+    it("contains an input to add todo items", () => {
+      const input = app.shadowRoot.querySelector("todo-input")
+      expect(input).to.exist
+    })
+    it("contains an empty list of todo items", () => {
+      const list = app.shadowRoot.querySelector("ul")
+      expect(list).to.exist
+      expect(list).lightDom.to.equal("")
     })
   })
   describe("With a title property", () => {
@@ -22,6 +34,26 @@ describe("App", () => {
     it("the h1 contains the title", () => {
       const title = app.shadowRoot.querySelector("h1")
       expect(title.innerHTML).to.contain(appName)
+    })
+  })
+
+  describe("Events", () => {
+    beforeEach(async () => {
+      app = await fixture(html`<todo-list></todo-list>`)
+    })
+    describe("@add", () => {
+      beforeEach(async () => {
+        const input = app.shadowRoot.querySelector("todo-input")
+        await fireEvent(input, "add", "toto")
+      })
+      it("the list contains now an item", () => {
+        const list = app.shadowRoot.querySelector("ul")
+        expect(list).lightDom.to.equal("<li><todo-item></todo-item></li>")
+      })
+      it("the item is initialised with the text passed in the event", () => {
+        const todoItem = app.shadowRoot.querySelector("todo-item")
+        expect(todoItem.item).to.eql({ id: 0, text: "toto", checked: false })
+      })
     })
   })
 })
