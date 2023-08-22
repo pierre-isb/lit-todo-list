01652c6d7e0f145034fff49dc57f743546d00086
Pierre Isabel <pierre.isabel@octo.com>
2023-08-22 19:03:31 +0200

    deploy application

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 
 export default {
-  input: "main.js",
+  input: "index.js",
   output: {
     file: "build/index.js",
     format: "umd",
   },
   plugins: [commonjs(), resolve()],
-}
+};
